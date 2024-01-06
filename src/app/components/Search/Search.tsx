48d91abd740f9eda6a96e80556f8f90e54ffa3dd
Oscar Puerto <oscar.puerto@sallinggroup.com>
2024-01-06 19:26:02 +0100

    text color

--- src/app/components/Search/Search.tsx
+++ src/app/components/Search/Search.tsx
@@ -39,7 +39,7 @@ export default function Search() {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" onChange={handleQueryChange} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  dark:placeholder-gray-400 dark:text-white" placeholder="Search..." required />
+        <input type="search" onChange={handleQueryChange} id="default-search" className="block w-full p-4 ps-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50  dark:placeholder-gray-400 dark:text-white" placeholder="Search..." required />
         <button type="button" onClick={handleSearch} className="text-white absolute end-2.5 bottom-2.5 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2  dark:hover:bg-gray-700">Search</button>
     </div>
 </div>
